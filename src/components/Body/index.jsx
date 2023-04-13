14a40478ef655192091a722734c660ef04f5b891
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-04-13 13:10:29 -0300

    :construction: add img frame youtube

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import Button from '../Button'
+
+import VideoImg from "../../assets/video-img.jpg"
 
 import './style.sass'
-import Button from '../Button'
 
 function Body() {
   return (
     <div className="main-body">
       <h1 className="main-title">Lobe Tour</h1>
       <span className="main-subtitle">Build your first machine learning model in ten<br/> minutes. No code or experience required.</span>
+      <div className="tour-video-container">
+        <div className="frame-youtube">
+          <img className="img-video" src={VideoImg} alt="imagem video" />
+        </div>
+      </div>
       <h1 className="main-text">Train your<br/> app with Lobe</h1>
       <Button />
     </div>
