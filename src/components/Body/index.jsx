0f138d97425939f9e33fab311a3671da25133ae4
monsor3s <64220885+monsor3s@users.noreply.github.com>
2023-07-02 20:46:02 -0300

    :construction: Add max width in img-video

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -7,17 +7,17 @@ import './style.sass'
 
 function Body() {
   return (
-    <body className="main-body">
-      <h1 className="main-title">Lobe <span className="section-heading">Tour</span></h1>
-      <span className="main-subtitle">Build your first machine learning model in ten<br/> minutes. No code or experience required.</span>
-      <div className="tour-video-container">
-        <div className="frame-youtube">
-          <img className="img-video" src={VideoImg} alt="imagem video" />
-        </div>
-      </div>
-      <h1 className="main-text">Train your<br/> app with Lobe</h1>
-      <Button className="-large" />
-    </body>
+    <section className="main-section">
+        <h1 className="main-title">Lobe <span className="section-heading">Tour</span></h1>
+        <span className="main-subtitle">Build your first machine learning model in ten<br/> minutes. No code or experience required.</span>
+          <div className="tour-video-container">
+            <div className="frame-youtube">
+              <img className="img-video" src={VideoImg} alt="imagem video" />
+            </div>
+          </div>
+        <h1 className="main-text">Train your<br/> app with Lobe</h1>
+        <Button className="-large" />
+    </section>
   )
 }
 
